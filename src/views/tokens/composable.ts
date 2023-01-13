65abe3c79d65195a77ad44bb9bf798eca8320426
duanjie-cmd <18870420982@139.com>
2023-01-13 16:18:42 +0800

    bug fix

--- src/views/tokens/composable.ts
+++ src/views/tokens/composable.ts
@@ -125,7 +125,10 @@ export const useTokensSelected = (
     if (baseDenomQuery && rmIbcPrefix(baseDenomQuery as string).length) {
         rmIbcPrefixBaseDenomQuery = rmIbcPrefix(baseDenomQuery as string);
     }
-    const searchTokenKey = ref((rmIbcPrefixBaseDenomQuery || '') + (baseDenomChainQuery || ''));
+    const initSearchTokenKey = rmIbcPrefixBaseDenomQuery
+        ? rmIbcPrefixBaseDenomQuery || '' + baseDenomChainQuery || ''
+        : undefined;
+    const searchTokenKey = ref<string | undefined>(initSearchTokenKey);
     const searchChain = ref<string | undefined>(chainIdQuery);
     const searchStatus = ref<TTokenType>(statusQuery);
     const tokenData = computed(() => {
--- src/views/transfers/composable.ts
+++ src/views/transfers/composable.ts
@@ -250,7 +250,10 @@ export const useRouteParams = () => {
     if (startTimestamp && endTimestamp) {
         dateRange.value = [dayjsUtc(startTimestamp * 1000), dayjsUtc(endTimestamp * 1000)];
     }
-    searchToken.value = (paramsBaseDenom || '') + (paramsBaseDenomChain || '');
+    const initSearchToken = paramsBaseDenom
+        ? paramsBaseDenom || '' + paramsBaseDenomChain || ''
+        : undefined;
+    searchToken.value = initSearchToken;
     if (paramsDenom && rmIbcPrefix(paramsDenom as string).length) {
         searchToken.value = rmIbcPrefix(paramsDenom as string);
         inputFlag.value = true;
